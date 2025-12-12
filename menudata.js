0446e755f4fce5527106ea3fc429c348ce33c4e4
AdarshKumarN <AdarshKumarN@users.noreply.github.com>
2025-12-12 11:12:44 +0000

    deploy: 85bcb5dff9dcea9d071a701eff04760cf304d031

--- menudata.js
+++ menudata.js
@@ -28,4 +28,6 @@ var menudata={children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
 {text:"All",url:"globals.html"},
-{text:"Functions",url:"globals_func.html"}]}]}]}
+{text:"Functions",url:"globals_func.html"}]}]},
+{text:"Main Page",url:"index.html"},
+{text:"Coverage Report",url:"coverage/index.html"}]}
