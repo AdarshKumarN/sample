6194695e9d76894d856a1dd39bd4178ec431ff59
AdarshKumarN <AdarshKumarN@users.noreply.github.com>
2025-12-12 10:48:25 +0000

    deploy: 79fb1b1a7026617c5b112680cfe7f278789ec28e

--- search/all_1.js
+++ search/all_1.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['main_0',['main',['../main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.cpp']]],
   ['main_2ecpp_1',['main.cpp',['../main_8cpp.html',1,'']]],
-  ['math_5futils_2eh_2',['math_utils.h',['../math__utils_8h.html',1,'']]],
-  ['multiply_3',['multiply',['../main_8cpp.html#a390660dbeb35c8538e7f24bfb067b2bc',1,'multiply(int a, int b):&#160;main.cpp'],['../math__utils_8h.html#a390660dbeb35c8538e7f24bfb067b2bc',1,'multiply(int a, int b):&#160;main.cpp']]]
+  ['math_5futils_2ecpp_2',['math_utils.cpp',['../math__utils_8cpp.html',1,'']]],
+  ['math_5futils_2eh_3',['math_utils.h',['../math__utils_8h.html',1,'']]],
+  ['multiply_4',['multiply',['../math__utils_8cpp.html#a390660dbeb35c8538e7f24bfb067b2bc',1,'multiply(int a, int b):&#160;math_utils.cpp'],['../math__utils_8h.html#a390660dbeb35c8538e7f24bfb067b2bc',1,'multiply(int a, int b):&#160;math_utils.cpp']]]
 ];
--- search/files_0.js
+++ search/files_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['main_2ecpp_0',['main.cpp',['../main_8cpp.html',1,'']]],
-  ['math_5futils_2eh_1',['math_utils.h',['../math__utils_8h.html',1,'']]]
+  ['math_5futils_2ecpp_1',['math_utils.cpp',['../math__utils_8cpp.html',1,'']]],
+  ['math_5futils_2eh_2',['math_utils.h',['../math__utils_8h.html',1,'']]]
 ];
